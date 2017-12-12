ed8541608e7cfc6a81f54ee5aee7b286d8bffd2c
Matthew Burgess <msappy00@yahoo.com>
2017-12-12 13:43:10 +0800

    fixes to game

--- js/blocked.js
+++ js/blocked.js
@@ -7,9 +7,7 @@ var blockArray = [[0, 0, 200, 150], [200, 0, 400, 150], [400, 0, 600, 150], [600
                   [0, 450, 200, 600], [200, 450, 400, 600], [400, 450, 600, 600], [600, 450, 800, 600], [800, 450, 1000, 600],
                   [0, 600, 200, 750], [200, 600, 400, 750], [400, 600, 600, 750], [600, 600, 800, 750], [800, 600, 1000, 750]];
 
-var points,
-image,
-blockCount
+var  ctx, image, points, blockCount
 
 function clearArray(){
     vocArray = [""];
@@ -18,48 +16,46 @@ function clearArray(){
 
 // preps the canvas and displays original image with variable resolution
 function init() {
-    canvas = document.getElementById('cv');
-    ctx = canvas.getContext('2d');
-    shuffleArray(blockArray);
+    points = 6;
+    blockCount = 26;
+    ctx = document.getElementById("cv").getContext("2d");
 }
 
 function setVoc(vocSet){
-    points = 5;
-    blockCount = 25;
     switch(vocSet) {
         case "AnimalsB3U1":
             vocArray = ["bird", "kitten", "puppy", "bunny", "turtle", "spider", "frog", "fish"];
             document.getElementById("vocSet").innerHTML = "Animals";
             break;
+        case "PlacesB6U5":
+            vocArray = ["farm", "airport", "port", "concert", "beach", "playground", "mountain", "amusement park"];
+            document.getElementById("vocSet").innerHTML = "Places";
+            break;
         default:
             vocArray = [""];
             break;
     }
+    shuffleArray(vocArray);
     shuffleArray(blockArray);
+    document.getElementById("points").innerHTML = "Points: " + points;
     unBlock();
 }
 
 function unBlock(){
-    document.getElementById("points").innerHTML = "Points: " + points;
-    var img = new Image();
-    img.src = "mes-flashcards/" + vocArray[array_i] + ".png";
-    img.onload = function(){
-        ctx.drawImage(img, 0, 0, 1000, 750);
-        // block the image
-        for(i = 0; i < blockCount; i++){
-            ctx.save();
-            ctx.beginPath();
+    var image = new Image();
+    image.src = 'mes-flashcards/' + vocArray[array_i] + '.png';
+    blockCount = blockCount - 5;
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0);
+        ctx.beginPath();
+        for (i=0; i<blockCount; i++){
             ctx.rect(blockArray[i][0], blockArray[i][1], blockArray[i][2], blockArray[i][3]);
-            ctx.fillStyle = "black";
-            ctx.fill();
-            ctx.restore();
         }
-        if (points > 0){
-            points -= 1;
-            blockCount -= 5;
-        } else {
-            points = 0;
-            blockCount = 0;
-        }
+        ctx.fill();
     }
+    if (points > 0){
+        points -= 1;
+    }
+    document.getElementById("points").innerHTML = "Points: " + points;
+
 }
