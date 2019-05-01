a763e0af27a12c5b932f2e7170f9fc8dc4bf74a9
msappy00 <mlburgess@gmail.com>
2019-05-02 00:32:39 +0800

    calendar and file cleanup

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,4 @@
 
-// these are labels for the days of the week
-cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 // these are human-readable month name labels, in order
 cal_months_labels = ['January', 'February', 'March', 'April',
                     'May', 'June', 'July', 'August', 'September',
@@ -29,42 +26,23 @@ Calendar.prototype.generateHTML = function(){
     var monthLength = monthLength(this.year, this.month);
     
     // do the header
-    var monthName = cal_months_labels[this.month]
-    var html = '<table class="calendar-table">';
-
-    html += '<tr><th colspan="7">';
-    html +=  '<h1>' + monthName + "&nbsp;" + this.year + '</h1>';
-    html += '</th></tr>';
-    html += '<tr class="calendar-header">';
-    for(var i = 0; i <= 6; i++ ){
-        html += '<td class="calendar-header-day">';
-        html += cal_days_labels[i];
-        html += '</td>';
+    var monthName = cal_months_labels[this.month];
+    var html = '<h1>' + monthName + "&nbsp;" + this.year + '</h1>';
+    html += '<div class="grid-container">';
+    html += '<div><h2>Sun</h2></div>';
+    html += '<div><h2>Mon</h2></div>';
+    html += '<div><h2>Tue</h2></div>';
+    html += '<div><h2>Wed</h2></div>';
+    html += '<div><h2>Thu</h2></div>';
+    html += '<div><h2>Fri</h2></div>';
+    html += '<div><h2>Sat</h2></div>';
+    for(var i = 0; i < startingDay; i++){
+        html += '<div></div>';
     }
-    html += '</tr><tr>';
-    
-    // fill in the days
-    var day = 1;
-    // this loop is for is weeks (rows)
-    for (var i = 0; i < 9; i++) {
-        // this loop is for weekdays (cells)
-        for (var j = 0; j <= 6; j++) { 
-        html += '<td class="calendar-day">';
-        if (day <= monthLength && (i > 0 || j >= startingDay)) {
-            html += day;
-            day++;
-        }
-        html += '</td>';
-        }
-        // stop making rows if we've run out of days
-        if (day > monthLength) {
-        break;
-        } else {
-        html += '</tr><tr>';
-        }
+    for(var i = 1; i <= monthLength; i++){
+        html += '<div><h2>' + i + '</h2></div>';
     }
-    html += '</tr></table>';
-    
+    html += '</div>';
     this.html = html;
 }
 
