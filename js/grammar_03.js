54f55a02b519fdb20fa63f94632741875a017eb5
msappy00 <mlburgess@gmail.com>
2019-06-23 12:24:33 +0800

    fixed grammar L3

--- js/grammar_03.js
+++ js/grammar_03.js
@@ -253,38 +253,34 @@ function g_03(userInput){
         gLinks = [''];
         grammarLesson = "";
     } else if (userInput == "Unit 9") {
-        pronounArray = ["I", "you", "we", "they", "he", "she"];
-        weatherArray = [({weather:'sunny', pic:'<img src="Level 3/images/sunny.png" />'}), ({weather:'rainy', pic:'<img src="Level 3/images/rainy.png" />'}), ({weather:'cloudy', pic:'<img src="Level 3/images/cloudy.png" />'}), ({weather:'windy', pic:'<img src="Level 3/images/windy.png" />'}), ({weather:'snowy', pic:'<img src="Level 3/images/snowy.png" />'})];
-        verbArray = [({verb:'shopping', pic:'<img src="Level 3/images/go shopping.png" />'}), ({verb:'swimming', pic:'<img src="Level 3/images/go swimming.png" />'}), ({verb:'hiking', pic:'<img src="Level 3/images/go hiking.png" />'}), ({verb:'fishing', pic:'<img src="Level 3/images/go fishing.png" />'}), ({verb:'skateboarding', pic:'<img src="Level 3/images/go skateboarding.png" />'}), ({verb:'jogging', pic:'<img src="Level 3/images/go jogging.png" />'})];
-        shuffleArray(pronounArray);
-        shuffleArray(weatherArray);
+        randomG = randG(2);
+        PRPArray = [{PRP: "you", RESPONSE: "I"}, {PRP: "you", RESPONSE: "we"}, {PRP: "they", RESPONSE: "they"}, {PRP: "he", RESPONSE: "he"}, {PRP: "she", RESPONSE: "she"}];
+        shuffleArray(PRPArray);
+        ADJArray = [{ADJ:'sunny', pic:"Level 3/images/sunny.png"}, {ADJ:'rainy', pic:"Level 3/images/rainy.png"}, {ADJ:'cloudy', pic:"Level 3/images/cloudy.png"}, {ADJ:'windy', pic:"Level 3/images/windy.png"}, {ADJ:'snowy', pic:"Level 3/images/snowy.png"}];
+        shuffleArray(ADJArray);
+        verbArray = [{verb:'go shopping', pic:"Level 3/images/go shopping.png"}, {verb:'go swimming', pic:"Level 3/images/go swimming.png"}, {verb:'go hiking', pic:"Level 3/images/go hiking.png"}, {verb:'go fishing', pic:"Level 3/images/go fishing.png"}, {verb:'go skateboarding', pic:"Level 3/images/go skateboarding.png"}, {verb:'go jogging', pic:"Level 3/images/go jogging.png"}];
         shuffleArray(verbArray);
-        document.getElementById("g0r0d0").innerHTML = "How's the weather?";
-        document.getElementById("g0r0d5").innerHTML = weatherArray[0].pic;
-        sampleAnswer1 = "It's " + weatherArray[0].weather + ".";
-        if (pronounArray[0] == "he" || pronounArray[0] == "she"){
-            document.getElementById("g0r3d0").innerHTML = "What does " + pronounArray[0] + " like to do on " + weatherArray[0].weather + " days?";
+        if (randomG < 1){
+            document.getElementById("gPic").src = ADJArray[0].pic;
+            sentence = "How's the weather today?"
+            document.getElementById("grammarCheck").value = "It's " + ADJArray[0].ADJ + ".";
+            sentenceArray[0] = "It's " + ADJArray[0].ADJ + ".";
+            sentenceArray[1] = "It's " + ADJArray[1].ADJ + ".";
+            sentenceArray[2] = "It's " + ADJArray[2].ADJ + ".";
+            sentenceArray[3] = "It's " + ADJArray[3].ADJ + ".";
         } else {
-            document.getElementById("g0r3d0").innerHTML = "What do " + pronounArray[0] + " like to do on " + weatherArray[0].weather + " days?";
-        }
-        var capitalized = pronounArray[0].charAt(0).toUpperCase() + pronounArray[0].slice(1);
-        if (pronounArray[0] == "he" || pronounArray[0] == "she"){
-            sampleAnswer2 = capitalized + " likes to go " + verbArray[0].verb + " on " + weatherArray[0].weather + " days.";
-        } else {
-            if (capitalized == "You"){
-                capitalized = "I";
-            } else if (capitalized == "I" || capitalized == "We"){
-                capitalized = "You";
+            document.getElementById("gPic").src = verbArray[0].pic;
+            verb = conjugate_VB(PRPArray[0].RESPONSE, "doVerb");
+            sentence = "What " + verb + " " + PRPArray[0].PRP +  " like to do on " + ADJArray[0].ADJ + " days?";
+            verb = conjugate_VB(PRPArray[0].RESPONSE, "likeVerb");
+            document.getElementById("grammarCheck").value = capitalize(PRPArray[0].RESPONSE) + " " + verb + " to " + verbArray[0].verb + " on " + ADJArray[0].ADJ + " days.";
+                sentenceArray[0] = capitalize(PRPArray[0].RESPONSE) + " " + verb + " to " + verbArray[0].verb + " on " + ADJArray[0].ADJ + " days.";
+                sentenceArray[1] = capitalize(PRPArray[0].RESPONSE) + " " + verb + " to " + verbArray[1].verb + " on " + ADJArray[0].ADJ + " days.";
+                sentenceArray[2] = capitalize(PRPArray[0].RESPONSE) + " " + verb + " to " + verbArray[2].verb + " on " + ADJArray[0].ADJ + " days.";
+                sentenceArray[3] = capitalize(PRPArray[0].RESPONSE) + " " + verb + " to " + verbArray[3].verb + " on " + ADJArray[0].ADJ + " days.";
             }
-            sampleAnswer2 = capitalized + " like to go " + verbArray[0].verb + " on " + weatherArray[0].weather + " days.";
-        }
-        
-        document.getElementById("g0r1d0").innerHTML = '<input style="width:500px" type="text" id="grammarInput" />';
-        document.getElementById("g0r1d1").align = "left";
-        document.getElementById("g0r1d1").innerHTML = '<button class="w3-button w3-blue w3-round" onclick="showGrammar1()">Example</button>';
-        document.getElementById("g0r4d0").innerHTML = '<input style="width:500px" type="text" id="grammarInput2" />';
-        document.getElementById("g0r4d1").align = "left";
-        document.getElementById("g0r4d1").innerHTML = '<button class="w3-button w3-blue w3-round" onclick="showGrammar2()">Example</button>';
+        setTimeout(function(){speak(sentence)}, 2000);
+        gLinks = [''];
         grammarLesson = "";
     } else if (userInput == "Review 3") {
         unitArray = ["Unit 7", "Unit 8", "Unit 9"];
