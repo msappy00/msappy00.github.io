a1957717dfd350ffdaded1f04d66f5da702b7b1d
msappy00 <mlburgess@gmail.com>
2019-10-03 09:06:51 +0800

    fixed grammar L8U1

--- js/grammar_08.js
+++ js/grammar_08.js
@@ -1,6 +1,45 @@
 function g_08(userInput){
     if (userInput == "Unit 1") {
-        
+        randomG = randG(4);
+        PRPArray = [{PRP: "I", RESPONSE: "me", OBJ: "you"}, {PRP: "you", RESPONSE: "you", OBJ: "me"}, {PRP: "you", RESPONSE: "us", OBJ: "us"}, {PRP: "they", RESPONSE: "them", OBJ: "them"}, {PRP: "he", RESPONSE: "him", OBJ: "him"}, {PRP: "she", RESPONSE: "her", OBJ: "her"}, {PRP: "we", RESPONSE: "you", OBJ: "you"}];
+        shuffleArray(PRPArray);
+        PERSONArray = ["My mom", " My dad", "Their son", "Their daughter", "His wife", "Her husband"];
+        shuffleArray(PERSONArray);
+        NOUNArray = [" the cake", " the pen", " the sandwich", " the chocolate", " the markers", " the toy", " the model car", " the doll", " the pencil", " the book", " the school bag", " the notebook", " the eraser", " the pencil case", " the hat", " the belt"];
+        shuffleArray(NOUNArray);
+        if (randomG == 0) {
+            document.getElementById("gPic").src = "Level 8/images/give to.png";
+            sentence = "Who gave" + NOUNArray[0] + " to " + PRPArray[0].OBJ + "?";
+            document.getElementById("grammarCheck").value = PERSONArray[0] + " gave" + NOUNArray[0] + " to " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[0] = PERSONArray[0] + " gave" + NOUNArray[0] + " to " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[1] = PERSONArray[0] + " gave" + NOUNArray[1] + " to " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[2] = PERSONArray[0] + " gave" + NOUNArray[2] + " to " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[3] = PERSONArray[0] + " gave" + NOUNArray[3] + " to " + PRPArray[0].RESPONSE + ".";
+        } else if (randomG == 1) {
+            document.getElementById("gPic").src = "Level 8/images/bring for.png";
+            sentence = "Who brought" + NOUNArray[0] + " for " + PRPArray[0].OBJ + "?";
+            document.getElementById("grammarCheck").value = PERSONArray[0] + " brought" + NOUNArray[0] + " for " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[0] = PERSONArray[0] + " brought" + NOUNArray[0] + " for " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[1] = PERSONArray[0] + " brought" + NOUNArray[1] + " for " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[2] = PERSONArray[0] + " brought" + NOUNArray[2] + " for " + PRPArray[0].RESPONSE + ".";
+            sentenceArray[3] = PERSONArray[0] + " brought" + NOUNArray[3] + " for " + PRPArray[0].RESPONSE + ".";
+        } else if (randomG == 2) {
+            document.getElementById("gPic").src = "Level 8/images/give to.png";
+            sentence = "Who gave " + PRPArray[0].OBJ + " " + NOUNArray[0] + "?";
+            document.getElementById("grammarCheck").value = PERSONArray[0] + " gave " + PRPArray[0].RESPONSE + NOUNArray[0] + ".";
+            sentenceArray[0] = PERSONArray[0] + " gave " + PRPArray[0].RESPONSE + NOUNArray[0] + ".";
+            sentenceArray[1] = PERSONArray[0] + " gave " + PRPArray[0].RESPONSE + NOUNArray[1] + ".";
+            sentenceArray[2] = PERSONArray[0] + " gave " + PRPArray[0].RESPONSE + NOUNArray[2] + ".";
+            sentenceArray[3] = PERSONArray[0] + " gave " + PRPArray[0].RESPONSE + NOUNArray[3] + ".";
+        } else {
+            document.getElementById("gPic").src = "Level 8/images/bring for.png";
+            sentence = "Who brought " + PRPArray[0].OBJ + " " + NOUNArray[0] + "?";
+            document.getElementById("grammarCheck").value = PERSONArray[0] + " brought " + PRPArray[0].RESPONSE + NOUNArray[0] + ".";
+            sentenceArray[0] = PERSONArray[0] + " brought " + PRPArray[0].RESPONSE + NOUNArray[0] + ".";
+            sentenceArray[1] = PERSONArray[0] + " brought " + PRPArray[0].RESPONSE + NOUNArray[1] + ".";
+            sentenceArray[2] = PERSONArray[0] + " brought " + PRPArray[0].RESPONSE + NOUNArray[2] + ".";
+            sentenceArray[3] = PERSONArray[0] + " brought " + PRPArray[0].RESPONSE + NOUNArray[3] + ".";
+        }
         setTimeout(function(){speak(sentence)}, 2000);
         gLinks = [''];
     } else if (userInput == "Unit 2") {
