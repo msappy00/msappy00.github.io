a5b17fc87c5bd0c99d0f78f063665dc0a627bbc7
msappy00 <mlburgess@gmail.com>
2022-07-17 01:45:51 +0800

    api rework

--- js/weather_api.js
+++
@@ -1,9 +0,0 @@
-function load_weather(form) {
-    ow_key = form[0].value;
-    window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-    window.myWidgetParam.push({id: 11,cityid: '1673820',appid: ow_key,units: 'metric',containerid: 'openweathermap-widget-11',  });
-    (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";
-    script.src = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-    var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);
-      })();
-}
