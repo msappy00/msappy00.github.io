3e1f494190a7589d8547733d28317da8569da644
Matthew Burgess <msappy00@users.noreply.github.com>
2022-11-29 10:03:24 +0800

    Update listening.js

--- Listening/listening.js
+++ Listening/listening.js
@@ -11,16 +11,16 @@ if ('serviceWorker' in navigator) {
 }
 
 const listening = {
-    question1: "Why did the teacher talk to Mr. Baxter?",
-    choiceA: "he was watching TV",
-    choiceB: "he was not focusing on class",
-    choiceC: "he was paying attention",
-    answer1: "He was not focusing on class.",
-    question2: "What is their homework about?",
-    choiceD: "Mr. Baxter",
-    choiceE: "heroes",
-    choiceF: "the teacher",
-    answer2: "Their homework is about heroes."
+    question1: "Good listening is ",
+    choiceA: "one of the worst things we can do.",
+    choiceB: "one of the most important things we can do.",
+    choiceC: "the last thing we should do.",
+    answer1: "Good listening is one of the most important things we can do.",
+    question2: "To become a better listener you should ",
+    choiceD: "put your phone away.",
+    choiceE: "turn on the TV.",
+    choiceF: "turn up your headphones.",
+    answer2: "To become a better listener you should put your phone away."
   };
 
 function setContent() {
