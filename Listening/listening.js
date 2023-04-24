d68ec929fd360051e52a77e9aa230a6a5af7e2e6
Matthew Burgess <msappy00@users.noreply.github.com>
2023-04-24 11:09:25 +0000

    Update listening.js

--- Listening/listening.js
+++ Listening/listening.js
@@ -1,13 +1,10 @@
 // check if service worker is supported
 if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker.register("listening.js", {scope: '/Listening/' }).then(function(reg) {
-    console.log('Successfully registered. Scope is ' + reg.scope);
-    }).catch(function(error) {
-        console.log('Registering failed ' + error);
-    });
+    navigator.serviceWorker.register("listening.js", { scope: '/Listening/' })
+    .then(reg => console.log(Successfully registered. Scope is ${reg.scope}))
+    .catch(error => console.log(Registering failed ${error}));
 } else {
-    console.log('Service worker can not be registered on this device');
+    console.log('Service worker cannot be registered on this device');
 }
 
 const listening = {
