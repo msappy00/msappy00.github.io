74fef0ef259fcbbaee7ec264f2bc588bfd73b0fc
Matthew Burgess <msappy00@users.noreply.github.com>
2023-11-24 09:10:29 +0800

    new questions

--- script.js
+++ script.js
@@ -2,32 +2,95 @@
 
 const Questions = [
 	{
-	  q: "What country is considered to be the birthplace of coffee?",
+	  q: "What is the main goal of Fairtrade?",
 	  a: [
-		{ text: "Ethiopia", isCorrect: true },
-		{ text: "Brazil", isCorrect: false },
-		{ text: "Colombia", isCorrect: false },
-		{ text: "Italy", isCorrect: false },
+		{ text: "To make coffee more affordable.", isCorrect: false },
+		{ text: "To reduce the amount of coffee produced.", isCorrect: false },
+		{ text: "To increase the price of coffee.", isCorrect: false },
+		{ text: "To improve the lives of poor producers.", isCorrect: true },
 	  ],
 	},
 	{
-	  q: "Which type of coffee bean is considered to be the most expensive?",
+	  q: "What is a smallholder coffee farmer?",
 	  a: [
-		{ text: "Kopi Luwak", isCorrect: true },
-		{ text: "Arabica", isCorrect: false },
-		{ text: "Robusta", isCorrect: false },
-		{ text: "Liberica", isCorrect: false },
+		{ text: "A farmer who makes a small profit.", isCorrect: false },
+		{ text: "A farmer who owns a small farm.", isCorrect: true },
+		{ text: "A farmer who uses cheap pesticides.", isCorrect: false },
+		{ text: "A farmer who produces a small crop.", isCorrect: false },
 	  ],
 	},
 	{
-	  q: "What is the most popular way to make coffee in the United States?",
+	  q: "What are some of the challenges faced by smallholder coffee farmers?",
 	  a: [
-		{ text: "Drip coffee", isCorrect: true },
-		{ text: "Espresso", isCorrect: false },
-		{ text: "French press", isCorrect: false },
-		{ text: "Pour-over", isCorrect: false },
+		{ text: "They earn less than they need to survive.", isCorrect: false },
+		{ text: "They lack infrastructure such as roads, schools, and hospitals.", isCorrect: false },
+		{ text: "They use cheap pesticides that damage the environment.", isCorrect: false },
+		{ text: "All of the above.", isCorrect: true },
 	  ],
 	},
+	{
+		q: "How does Fairtrade help smallholder coffee farmers?",
+		a: [
+		  { text: "By cutting out the middlemen and buying straight from farmers.", isCorrect: false },
+		  { text: "By helping farmers build infrastructure in their communities.", isCorrect: false },
+		  { text: "By introducing farmers to eco-friendly farming practices.", isCorrect: false },
+		  { text: "All of the above.", isCorrect: true },
+		],
+	  },
+	  {
+		q: "What are some of the benefits of Fairtrade for smallholder coffee farmers?",
+		a: [
+		  { text: "They earn more money from their coffee beans.", isCorrect: false },
+		  { text: "Their communities are stronger and more sustainable.", isCorrect: false },
+		  { text: "They are able to grow organic coffee, which is more profitable.", isCorrect: false },
+		  { text: "All of the above.", isCorrect: true },
+		],
+	  },
+	  {
+		q: "What are some of the benefits of Fairtrade for consumers?",
+		a: [
+		  { text: "They can help improve the lives of poor producers.", isCorrect: false },
+		  { text: "They can buy coffee that is produced in a sustainable way.", isCorrect: false },
+		  { text: "They can buy coffee that is more affordable.", isCorrect: false },
+		  { text: "All of the above.", isCorrect: true },
+		],
+	  },
+	  {
+		q: "What is the difference between Fairtrade and organic coffee?",
+		a: [
+			{ text: "Fairtrade coffee is more expensive than organic coffee.", isCorrect: false },
+			{ text: "Organic coffee is produced in a sustainable way, while Fairtrade coffee is not.", isCorrect: false },
+			{ text: "Organic coffee is more expensive than Fairtrade coffee.", isCorrect: false },
+			{ text: "Fairtrade coffee is produced in a sustainable way, while organic coffee is not.", isCorrect: true },	
+		],
+	  },
+	  {
+		q: "Which coffee brand is Fairtrade-certified?",
+		a: [
+		  { text: "Taipei Coffee", isCorrect: false },
+		  { text: "OKOGREEN", isCorrect: false },
+		  { text: "Formosa Coffee", isCorrect: false },
+		  { text: "All of the above.", isCorrect: true },
+		],
+	  },
+	  {
+		q: "How can I help support Fairtrade?",
+		a: [
+		  { text: "Buy Fairtrade-certified coffee.", isCorrect: false },
+		  { text: "Donate to Fairtrade.", isCorrect: false },
+		  { text: "Volunteer for Fairtrade.", isCorrect: false },
+		  { text: "All of the above.", isCorrect: true },
+		],
+	  },
+	  {
+		q: "What is the future of Fairtrade?",
+		a: [	
+			{ text: "Fairtrade will continue to grow and help more coffee farmers.", isCorrect: true },
+			{ text: "Fairtrade will decline in popularity.", isCorrect: false },
+			{ text: "Fairtrade will be replaced by a new organization.", isCorrect: false },
+			{ text: "None of the above.", isCorrect: false },
+		],
+	  },
   ];
 
 let currQuestion = 0
