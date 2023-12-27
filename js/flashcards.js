9d2437590bca6d1bb16a0fe12f33c1225b72a899
Matthew Burgess <msappy00@users.noreply.github.com>
2023-12-27 15:03:49 +0800

    new lesson

--- js/flashcards.js
+++ js/flashcards.js
@@ -10,7 +10,7 @@ function showNextCard() {
     return;
   }
   document.getElementById('flashcard-question').textContent = currentCard.question;
-  document.getElementById('flashcard-audio').setAttribute('src', 'audio/' + currentCard.answer + '.mp3');
+  // document.getElementById('flashcard-audio').setAttribute('src', 'audio/' + currentCard.answer + '.mp3');
     document.getElementById('flashcard-answer').textContent = "";
     document.getElementById('answer-button').addEventListener('click', () => {
     document.getElementById('flashcard-answer').textContent = currentCard.answer;
@@ -44,26 +44,24 @@ function shuffle(array) {
 }
 
 // Load initial cards (replace with your data source)
-flashcards.push({ question: 'Listen and repeat:', answer: 'related' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'typically' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'globally' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'reliable' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'forward' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'innocent' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'term' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'continent' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'hunter' });
-flashcards.push({ question: 'Listen and repeat:', answer: 'visible' });
-flashcards.push({ question: '(動物)有親緣關係的(文中接 to N. 指「和………………有親緣關係」)；相關的', answer: 'related' });
-flashcards.push({ question: '一般、通常；典型地', answer: 'typically' });
-flashcards.push({ question: '全球地；在全球各地', answer: 'globally' });
-flashcards.push({ question: '可靠的、值得信賴的、確鑿的', answer: 'reliable' });
-flashcards.push({ question: '向前、朝前', answer: 'forward' });
-flashcards.push({ question: '天真的；無辜的；清白的', answer: 'innocent' });
-flashcards.push({ question: '措詞、詞語；術語、專門名稱', answer: 'term' });
-flashcards.push({ question: '洲；大陸', answer: 'continent' });
-flashcards.push({ question: '狩獵者、獵人', answer: 'hunter' });
-flashcards.push({ question: '肉眼可見的；明顯的', answer: 'visible' });
+flashcards.push({ question: 'What is the name of the animal that the coffee farmers in Colombia are working to protect?', answer: 'the Andean bear' });
+flashcards.push({ question: 'What is the other name for the Andean bear, based on its facial markings?', answer: 'the spectacled bear' });
+flashcards.push({ question: 'What continent is the Andean bear native to?', answer: 'South America' });
+flashcards.push({ question: 'What is the main role of the Andean bear in the local ecosystem?', answer: 'to help spread various plant seeds around the environment' });
+flashcards.push({ question: 'What is the main food source of the Andean bear?', answer: 'plants' });
+flashcards.push({ question: 'What is the name of the mountain range that the Andean bear depends on for food and shelter?', answer: 'the Andes Mountains' });
+flashcards.push({ question: 'What is the main reason for the loss of the Andean bear\’s habitat?', answer: 'the clearing of land for coffee farms' });
+flashcards.push({ question: 'What is the main reason for the hunting of the Andean bear by the farmers?', answer: 'to protect their source of income from the bears\’ crop and cattle raiding' });
+flashcards.push({ question: 'How many Andean bears are estimated to be left in the wild?', answer: 'about 10,000' });
+flashcards.push({ question: 'What is the name of the program that was created to educate people about the Andean bear and encourage conservation?', answer: 'Conservamos la Vida' });
+flashcards.push({ question: 'What does the name of the program mean in English?', answer: 'conserving life' });
+flashcards.push({ question: 'What are some of the benefits that the program provides to the people who participate in it?', answer: 'the materials they need for coffee growing and processing, feeding their animals, and creating fences to keep bears out' });
+flashcards.push({ question: 'What are some of the measures that the farmers take to prevent the bears from entering their farms?', answer: 'creating fences and leaving large areas of land untouched for the bears' });
+flashcards.push({ question: 'What is the name of the joint business that the local farmers formed to sell their coffee?', answer: 'Café Oso Andino' });
+flashcards.push({ question: 'What is the name of the coffee brand that is inspired by the Andean bear?', answer: 'Andean Bear Coffee' });
+flashcards.push({ question: 'What is the main outcome of the joint business for the local farmers?', answer: 'bringing more wealth into the region' });
+flashcards.push({ question: 'How does protecting the Andean bear and the local forests affect the coffee business?', answer: 'positively' });
+flashcards.push({ question: 'What is the main message of Conservamos la Vida?', answer: 'that it is possible for conservation and business to work together successfully' });
 
 // Shuffle the cards
 shuffle(flashcards);
